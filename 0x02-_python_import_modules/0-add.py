3095adafc3c87196759fee96b5bf35fe27bcca34
fraol <fraoltes22@gmail.com>
2021-11-23 23:38:06 -0800

    Program imports function

---
+++ 0x02-_python_import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+# 0_addpy
+# Fraol Tesfaye
+if _name_== "_main_":
+    """print the sum of 1 and 2."""
+    from and_0 import add
+    a = 1
+    b = 2
+    print("{}+{}={}". format(a,b, add(a, b,)))
