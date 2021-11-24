ab740bdbbb5366a8e2010527f2cd37642d47bece
fraol <fraoltes22@gmail.com>
2021-11-24 04:53:04 -0800

    Program that prints the number and the list of its argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    argc = len(sys.argv) - 1
+    i = 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
