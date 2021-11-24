30d661ee142f3a6dfe3d61f9dd49fdafff29ab4d
fraol <fraoltes22@gmail.com>
2021-11-24 03:01:38 -0800

    Hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
-#!/user/bin/python3
-# 0_addpy
-# Fraol Tesfaye
-if _name_== "_main_":
-    """print the sum of 1 and 2."""
-    from and_0 import add
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{}+{}={}". format(a,b, add(a, b,)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
