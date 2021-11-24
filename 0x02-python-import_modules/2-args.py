80826fd92310d30a47482865b53988ea07052187
fraol <fraoltes22@gmail.com>
2021-11-24 04:59:11 -0800

    Hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,3 +12,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
+ for arg in sys.argv[1:]:
+        print("{}: {}".format(i, arg))
+        i += 1
