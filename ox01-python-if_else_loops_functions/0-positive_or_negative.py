decea58f2e5892429c748ef856db435fd28a1dae
fraol <fraoltes22@gmail.com>
2021-12-16 07:53:26 -0800

    Program that assign a random number and print sign of the number

---
+++ ox01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = ramdom.randint(-10, 10)
+if number >= 0:
+    print('{} is positive\n'.format(number))
+
+elif number == 0:
+    print('{} is zero\n'.format(number))
+
+else:
+    print('{} is negetive\n')
