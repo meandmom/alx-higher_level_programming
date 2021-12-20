98e44bc74321ffcbe638cd01fc1e845b04480f10
fraol <fraoltes22@gmail.com>
2021-12-20 07:19:24 -0800

    Program that assign random number and prints last digit and a string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ else:
     n = number % 10
 
 if n > 5:
-    print("Last digit of {:d} is {:d} and is grater than 5".format(number, n))
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, n))
 
 elif n == 0:
-    print('Last digit of {} is {} amd is 0'.format(number, n))
+    print('Last digit of {} is {} and is 0'.format(number, n))
 
 else:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, n))
