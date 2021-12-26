f9494f5c3fbba584c0c3b816920f1a28b9063ced
fraol <fraoltes22@gmail.com>
2021-12-26 01:15:52 -0800

    Function that prints a strin in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    x = ''
+    for a in str:
+        b = ord(a)
+        if b >= 32 & b <= 122:
+            if 32 <= b < 65:
+                x += a
+            elif 65 <= b <= 90:
+                x += chr(b)
+            else:
+                x += chr(b - 32)
+    return x
