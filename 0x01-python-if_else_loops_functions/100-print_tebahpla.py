bac04a69cb0aa5beae3dce0e37cda52e4355db94
fraol <fraoltes22@gmail.com>
2021-12-31 03:36:59 -0800

    Program that prints alphabet in reverse order alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for n in range(122, 96, -1):
     if n % 2 == 0:
         print("{}".format(chr(n)), end="")
     else:
-        print("{}".format(chr(n -32)), end="")
+        print("{}".format(chr(n - 32)), end="")
